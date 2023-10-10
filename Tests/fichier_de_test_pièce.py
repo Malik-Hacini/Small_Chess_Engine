1465937568f6be3ff68401ef28a4acfae4cbc6b6
Concombrero <boyer.tim.travail@gmail.com>
2023-10-10 10:39:31 +0200

    Test Reine plus cache

--- Tests/fichier_de_test_pièce.py
+++ Tests/fichier_de_test_pièce.py
@@ -1,14 +1,14 @@
 import pytest
 
 from joueurs import*
-from Partie import*
+from EtatJeu import*
 from piece import*
 
 
 def init_partie_test(nom_test):
     j1=Joueur("J1", True)
     j2=Joueur("J2", False)
-    partie=Partie(j1,j2, nom_test)
+    partie=EtatJeu(j1,j2, nom_test)
     print(partie)
     return  j1, j2, partie
 
@@ -31,4 +31,4 @@ def test_deplacement_tour():
 
 def test_deplacement_reine():
     j1, j2, partie=init_partie_test("test_deplacement_reine")
-    assert j1.pieces[0].coups_possibles(partie)==[(3,5)]
+    assert j1.pieces[0].coups_possibles(partie)==[(5, 4), (6, 5), (7, 6), (5, 2), (6, 1), (7, 0), (3, 2), (3, 4), (2, 5), (5, 3), (4, 4), (4, 5), (4, 6), (4, 7), (3, 3), (2, 3), (1, 3), (0, 3), (4, 2), (4, 1)]
