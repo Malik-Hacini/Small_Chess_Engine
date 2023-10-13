c2057f8a5e20e2bcb9b5152357cec8d759b120b6
Concombrero <boyer.tim.travail@gmail.com>
2023-10-13 12:33:12 +0200

    Merge du main ntm

---
+++ Tests/test_valeur.py
@@ -0,0 +1,78 @@
+import pytest
+import os
+
+from joueurs import*
+from EtatJeu import*
+from piece import*
+
+def init_partie_test(nom_test):
+    chemin_fichier = os.path.join("Sauvegarde_Test_Valeur", nom_test)
+    partie=EtatJeu(sauvegarde=chemin_fichier)
+    print(partie)
+    return partie
+
+def test_valeur_initiale():
+    partie=init_partie_test("test_valeur_initiale")
+    partie=init_partie_test("test_valeur_initiale")
+    partie.calcul_valeur()
+    assert partie.valeur==0
+    
+def test_valeur_avantage_materiel():
+    #Avantage Blanc
+    partie=init_partie_test("test_avantage_blancs_Reine")
+    partie.calcul_valeur()
+    assert partie.valeur==8.9
+    partie=init_partie_test("test_avantage_blancs_Tour")
+    partie.calcul_valeur()
+    assert partie.valeur==5
+    partie=init_partie_test("test_avantage_blancs_Fou")
+    partie.calcul_valeur()
+    assert partie.valeur==2.9
+    partie=init_partie_test("test_avantage_blancs_Cavalier")
+    partie.calcul_valeur()
+    assert partie.valeur==2.9
+    partie=init_partie_test("test_avantage_blancs_Pion")
+    partie.calcul_valeur()
+    assert partie.valeur==0.6
+    
+    #Avantage Noir
+    partie=init_partie_test("test_avantage_noirs_Reine")
+    partie.calcul_valeur()
+    assert partie.valeur==-8.9
+    partie=init_partie_test("test_avantage_noirs_Tour")
+    partie.calcul_valeur()
+    assert partie.valeur==-5
+    partie=init_partie_test("test_avantage_noirs_Fou")
+    partie.calcul_valeur()
+    assert partie.valeur==-2.9
+    partie=init_partie_test("test_avantage_noirs_Cavalier")
+    partie.calcul_valeur()
+    assert partie.valeur==-2.9
+    partie=init_partie_test("test_avantage_noirs_Pion")
+    partie.calcul_valeur()
+    assert partie.valeur==-0.6
+    
+def test_valeur_cases_controllees():
+    partie=init_partie_test("test_valeur_cases_controllees_Tour")
+    partie.calcul_valeur()
+    assert partie.valeur==6.2
+    partie=init_partie_test("test_valeur_cases_controllees_Fou")
+    partie.calcul_valeur()
+    assert partie.valeur==-3.7
+
+def test_valeur_position():
+    partie=init_partie_test("test_valeur_centre_blancs")
+    partie.calcul_valeur()
+    assert partie.valeur==1.35
+    partie=init_partie_test("test_valeur_centre_noirs")
+    partie.calcul_valeur()
+    assert partie.valeur==-1.35
+    partie=init_partie_test("test_valeur_sous_centre_blancs")
+    partie.calcul_valeur()
+    assert partie.valeur==1.25
+    partie=init_partie_test("test_valeur_sous_centre_noirs")
+    partie.calcul_valeur()
+    assert partie.valeur==-1.25
+    
+def test_valeur_pions_doubles():
+    pass
