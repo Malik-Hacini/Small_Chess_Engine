3df8a0f0fb25780c6ba8ebb28700268691c30d04
Zertag <juioiplm@gmail.com>
2023-11-14 17:42:40 +0100

    test etat_jeu

---
+++ Tests/Autre/test_EtatJeu.py
@@ -0,0 +1,52 @@
+import pytest
+import os
+from EtatJeu import * 
+
+
+def init_partie_test(nom_test):
+    """Fonction qui initialise une partie à partir d'un plateau de test donné
+
+    Args:
+        nom_test (str): nom du fichier sauvegarde de test
+
+    Returns:
+        EtatJeu: L'état de la partie
+    """
+    chemin_fichier = os.path.join("Sauvegarde_Test_EtatJeu", nom_test)
+    partie=EtatJeu(sauvegarde=chemin_fichier)
+    print(partie)
+    return partie
+
+
+def test_pat():
+    partie=init_partie_test("pat_1")
+    assert partie.pat()
+    partie=init_partie_test("pat_2")
+    partie.pieces[1][0].odometre=40
+    assert partie.pat()
+    
+def test_echec():
+    partie=init_partie_test("avec_echec")
+    assert partie.echec()
+    partie=init_partie_test("sans_echec")
+    assert not partie.echec()
+
+def test_mat():
+    partie=init_partie_test("avec_mat")
+    assert partie.echec_et_mat()
+    partie=init_partie_test("sans_mat")
+    assert not partie.echec_et_mat()
+    
+#Test valeur:
+def test_valeur():
+    #Test de situation finale
+    partie=init_partie_test("avec_mat")
+    assert partie.calcul_valeur()==-1000
+    partie=init_partie_test("pat_1")
+    assert partie.calcul_valeur()==0
+    #Test centre et sous centre
+    partie=init_partie_test("controle_centre_et_ss_centre")
+    assert partie.calcul_valeur()==0.4
+    #Test pions allignés
+    partie=init_partie_test("pions_allignes")
+    assert partie.calcul_valeur()==-0.1
